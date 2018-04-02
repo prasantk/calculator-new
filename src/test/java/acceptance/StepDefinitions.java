6ef667f38e94f946872ec3e6903b828402ea53ac
prasantk <publicapi@live.com>
2018-04-02 17:19:49 +0530

    debug 502 bad gateway issue

--- src/test/java/acceptance/StepDefinitions.java
+++ src/test/java/acceptance/StepDefinitions.java
@@ -26,7 +26,9 @@ public class StepDefinitions {
 
     @When("^the calculator sums them$")
     public void the_calculator_sums_them() throws Throwable {
+        System.out.println("Prasantk--");
         String url = String.format("%s/sum?a=%s&b=%s", server, a, b);
+        System.out.println("Prasant--",url);
         result = restTemplate.getForObject(url, String.class);
     }
 
